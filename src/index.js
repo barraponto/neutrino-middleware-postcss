79e071d8e195818e84110c73ce2d4db300273187
Capi Etheriel <barraponto@gmail.com>
2017-03-23 11:54:23 -0300

    feat(sourcemaps): generate sourcemaps if not in production

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-module.exports = (neutrino) => {
-  neutrino.config.module.rule('css')
-    .loader('postcss', require.resolve('postcss-loader'));
-  neutrino.config.module.rule('css')
+const POSTCSS_LOADER = require.resolve('postcss-loader');
+module.exports = ({ config }) => {
+  config.module.rule('css')
+    .loader('postcss', POSTCSS_LOADER, { map: process.env.NODE_ENV !== 'production' });
+  config.module.rule('css')
     .loader('css', ({ loader, options }) => ({
       loader,
       options: Object.assign({}, options, { importLoaders: 1 }),
